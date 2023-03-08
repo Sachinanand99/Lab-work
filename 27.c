b677572c5e4b13d4b7f83b297c734c006cc22f70
Sachin Anand <sachinanand10@hotmail.com>
2023-03-08 23:57:58 +0530

    Error of program fixes

--- 27.c
+++ 27.c
@@ -2,16 +2,16 @@
 int main(){
     float perc, prof, cp, sp;
     printf("Enter the cost price of item : ");
-    scanf("%d", &cp);
+    scanf("%f", &cp);
     printf("Enter the selling price of item. : ");
-    scanf("%d", &sp);
+    scanf("%f", &sp);
     if (cp>sp){
         prof = cp-sp;
-        printf("You have made loss of %d : ", prof);
+        printf("You have made loss of %.2f.\n", prof);
     }
     else{
         prof = sp-cp;
-        printf("You have made Profit of %d : ", prof);
+        printf("You have made Profit of %.2f.\n", prof);
     }
     perc = (prof/cp)*100;
     printf("Your percentage is %.2f.", perc);
