5af7ee69889d3ce46ca1259e12b19c71af9d5013
Sachin Anand <sachinanand10@hotmail.com>
2023-03-08 20:59:45 +0530

    Error of program fix

--- 14.c
+++ 14.c
@@ -5,7 +5,6 @@ int main(){
     scanf("%d", &num);
     dig1 = num%10;
     num = num/1000;
-    printf("%d%d", dig1, dig2);
     sum = dig1+num;
     printf("Sum of its first and its last digit is %d", sum);
     return 0;
